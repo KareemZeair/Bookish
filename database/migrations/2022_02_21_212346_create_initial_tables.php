515ae8907fa295a8b887b9d691a8b02799563032
KareemZeair <knzeair@mun.ca>
2022-02-24 23:59:55 -0330

    Initial models and migrations
    Co-authored-by: Youssef Ali <youssef-m-ali@users.noreply.github.com>

--- database/migrations/2022_02_21_212346_create_initial_tables.php
+++ database/migrations/2022_02_21_212346_create_initial_tables.php
@@ -17,8 +17,40 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            // $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('ISBN')->unique();
+            $table->string('author');
+            $table->text('plot');
+            $table->date('publish_date');
+        });
+
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('book_id');
+            $table->string('content');
+            $table->date('added_on');
+        });
+
+        Schema::create('wish_lists', function (Blueprint $table) {
+            $table->id();
+            $table->integer('book_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+
+        Schema::create('past_reads', function (Blueprint $table) {
+            $table->id();
+            $table->integer('book_id');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -31,5 +63,9 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
+        Schema::dropIfExists('reviews');
+        Schema::dropIfExists('wish_lists');
+        Schema::dropIfExists('past_reads');
     }
 };
